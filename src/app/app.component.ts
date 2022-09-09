23865e018da321516740cb71e76b48f51644a7e2
MuniBarbieri <edmundobarbieri@gmail.com>
2022-09-09 10:36:43 -0300

    * Install bootstrap Create a  table and  show  some ccys

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface Coin {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+  price_change_percentage_24: number;
+  total_volume: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,13 +18,19 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
+  coins: Coin[] = [];
+
   ngOnInit(): void {
     this.http
-      .get(
+      .get<Coin[]>(
         'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
       )
-      .subscribe((res) => {
-        console.log(res);
-      }, err=> console.log(err));
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.coins = res;
+        },
+        error: (err) => console.log(err),
+      });
   }
 }
