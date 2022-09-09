670dffb7c3bfc02e527a48ef214a1cb780f3bba5
MuniBarbieri <edmundobarbieri@gmail.com>
2022-09-09 09:15:46 -0300

    AppComponent: Test API ok. AppModule: Add HttpClientModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+      )
+      .subscribe((res) => {
+        console.log(res);
+      }, err=> console.log(err));
+  }
+}
